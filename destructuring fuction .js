31877b58e390d3200be248961da92086956cc194
MatheusDeZottis <matheusdezottis@gmail.com>
2024-07-10 21:20:33 -0300

    Tratando erros

--- destructuring fuction .js
+++ destructuring fuction .js
@@ -4,8 +4,12 @@ function rand({min = 0, max = 1000} = {}) {
 }
 
 const obj = { max: 50, min: 40}
+
 console.log(rand(obj))
+
 console.log(rand({min: 955}))
+
 console.log(rand({}))
+
 console.log(rand())
 
--- erro.js
+++ erro.js
@@ -1,1 +1,13 @@
-// tratando erros
+// tratando erros usando o try 
+
+function imprimirNomeGritado(obj) {
+    try{
+        console.log(obj.name.toUpperCase)() + '!!!'
+    } catch (e) {
+        tatarErroElancar(e)
+    }
+
+}
+    
+const obj = { nome: 'Roberto'}
+imprimirNomeGritado(obj)
