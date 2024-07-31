ae3506a512e966c4a0e5a8e89c21c3cd696b3ec0
MatheusDeZottis <matheusdezottis@gmail.com>
2024-07-31 19:34:16 -0300

    exemplos de fuction

---
+++ funcao/cidadaoprimeiraclasse.js
@@ -0,0 +1,41 @@
+
+
+// criar de forma literal
+
+function fun1(){
+
+}
+
+// Armazenar em uma variavel
+
+const fun2 = function () {
+
+}
+
+// Armazenar dentro de um array
+
+const array = [function (a, b) { return a + b } , fun1, fun2]
+console.log(array[0](2, 3))
+
+// armazenar em atributo de objeto
+
+const obj = {}
+obj.falar = function () { return "Opa"}
+console.log(obj.falar())
+
+// Passar função como paramentro
+
+function run(fun) {
+    fun()
+}
+run(function () { console.log('Executado')})
+
+// um função pode retornar/conter um função
+
+function soma (a, b) {
+    return function (c) {
+        console.log(a + b + c)
+    }
+}
+
+soma(2, 3)(4)
