defb7721cd964db682b65f559884fea0eb9495eb
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-10 14:54:40 -0300

    Criando uma fuction construtura

--- funcao/fuctioconstrutura.js
+++ funcao/fuctioconstrutura.js
@@ -2,6 +2,18 @@ function Carro (velocidadeDeMaxima = 200, delta = 5) {
     //atributo privado
     let velocidadeAtual = 0
    // metodo publico
+   this.acelerar = function() {
+    if(velocidadeAtual + delta <= velocidadeDeMaxima){
+        velocidadeAtual += delta
+        
+    } else{
+        velocidadeAtual = velocidadeDeMaxima
+    }
+   }
    
+   // metodo publico
+   this.getVelocidadeAtual = function(){
+    return velocidadeAtual
+   }
    }
  
