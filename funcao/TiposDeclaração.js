bf273f20a637a149eea60577328fd2701c907e52
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-21 07:23:42 -0300

    Tipos de declaração

---
+++ funcao/TiposDeclaração.js
@@ -0,0 +1,17 @@
+//fuction declatation, essa pode ser chamada com uma flexibilidade muito melhor de cima para baixo ou de vaixo para cima 
+
+function soma(x, y){
+    return x + y
+}
+
+// function expression
+
+const sub = function (x , y) {
+    return x - y
+}
+
+// named functio expression
+
+const mult = function mult(x, y) {
+    return x * y
+}
