6504ff0ee499b29db43aab4dcfb08b6668f6b7ea
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-25 12:38:43 -0300

    Estaciando valores

--- funcao/factory.js
+++ funcao/factory.js
@@ -1,4 +1,16 @@
-const prod1 = {
+/* const prod1 = {
     nome: '...',
     preco: 1234
+} */
+
+
+// factory simples
+
+function criarPessoa(){
+    return {
+        nome: 'ana',
+        sobrenome: 'Silva'
+    }
 }
+
+console.log(criarPessoa())
