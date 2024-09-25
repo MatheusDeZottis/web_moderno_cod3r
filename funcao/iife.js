e90d45673907627a0053cb28788523a7f1988e73
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-25 13:56:28 -0300

    IIfe => immediately Invoked Function Expression

---
+++ funcao/iife.js
@@ -0,0 +1,7 @@
+// IIfe => immediately Invoked Function Expression
+
+(function(){
+    console.log('Será executada na hora!')
+    console.log('Foge do escopo mais abrangente!')
+
+})() // isso é unico o que estiver aqui dentro está fora do escopo global 
