98ddf9c4e9abf4b8d0b8acc4286ac354b5e8eac0
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-25 13:20:15 -0300

    Exercicio

---
+++ funcao/factory2.js
@@ -0,0 +1,15 @@
+//exercicio
+
+
+const Desconto =  0.10 * 8.99
+
+
+function CriarProduto(){
+    
+    return {
+        nome: 'Mouse',
+        preco: 89.90 * Desconto 
+        
+    }
+}
+console.log(CriarProduto())
