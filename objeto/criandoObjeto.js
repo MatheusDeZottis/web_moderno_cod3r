abb75057167a82f825eeadba0b9fed70465e6cb1
MatheusDeZottis <matheusdezottis@gmail.com>
2024-11-14 13:09:56 -0300

    factore

--- objeto/criandoObjeto.js
+++ objeto/criandoObjeto.js
@@ -22,3 +22,18 @@ function produto(nome, preco, desc) {
 const p1 = new produto('Caneta', 7.99, 0.15)
 const p2 = new produto ('Notebook', 2298.98, 0.25)
 console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+function criarFucionario(nome, salarioBase, faltas) {
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario(){
+            return( salarioBase / 30) * (30 - faltas)
+        }
+    }
+}
+
+const f1 = criarFucionario('João', 7980, 4)
+const f2 = criarFucionario('Maria', 11400, 1)
+const f3 = criarFucionario(f1.getSalario(), f2.getSalario())
