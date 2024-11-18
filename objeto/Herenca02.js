4e69441d7deb9109c932f17337e2b523cb33927d
MatheusDeZottis <matheusdezottis@gmail.com>
2024-11-18 12:53:43 -0300

    Prototype chain

---
+++ objeto/Herenca02.js
@@ -0,0 +1,6 @@
+// cadeia de prptótipos (Prototype chain)
+
+const avo = { attr1: 'A'}
+const pai = { __proto__: avo, attr2: 'B'}
+const filho = { __proto__: pai, attr3: 'C'}
+console.log(filho.attr1)
