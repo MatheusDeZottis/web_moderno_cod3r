05d6c7919e3b34a8395955cf2ce7cf74d0f8ce4a
MatheusDeZottis <matheusdezottis@gmail.com>
2024-11-21 22:53:42 -0300

    Herança

--- objeto/Herenca02.js
+++ objeto/Herenca02.js
@@ -4,3 +4,16 @@ const avo = { attr1: 'A'}
 const pai = { __proto__: avo, attr2: 'B'}
 const filho = { __proto__: pai, attr3: 'C'}
 console.log(filho.attr1)
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324
+}
+
+const volvo = {
+    modelo: 'V40',
+    velMax: 240
+}
+
+console.log(ferrari)
+console.log(volvo)
