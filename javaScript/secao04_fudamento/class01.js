c0ef46746fcb8249f1d922929cbd817ad07d4265
MatheusDeZottis <matheusdezottis@gmail.com>
2024-12-17 22:20:30 -0300

    Trabalhando com class

---
+++ javaScript/secao04_fudamento/class01.js
@@ -0,0 +1,32 @@
+class Lacamento {
+    constructor(nome = "GEnerico", valor = 0){
+        this.nome = nome
+        this.valor = valor
+
+    }
+}
+
+class CicloFinaceiro {
+    constructor(mes,ano){
+        this.mes = mes
+        this.ano = ano
+        this.Lacamento = []
+    }
+    addLancamentos(...lancamentos) {
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+    sumario(){
+        let valorConsolidado = 0 
+        this.lancamentos.forEach( l => {
+            valorConsolidado += l.valor
+        })
+        return valorConsolidado
+    }
+}
+
+const salario = new Lacamento ('Salario', 45000)
+const contaDeLuz = new  Lacamento('Luz', -220)
+
+const contas = new CicloFinaceiro(6, 2018)
+contas.addLancamentos(salario, contaDeLuz)
+console.log(contas.sumario())
