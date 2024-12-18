d7f0a06fb3821336c862f36cd3c8c96b581125cf
MatheusDeZottis <matheusdezottis@gmail.com>
2024-12-17 22:35:01 -0300

    corrigindo o erro de acesso a propriedade

--- javaScript/secao04_fudamento/class01.js
+++ javaScript/secao04_fudamento/class01.js
@@ -1,32 +1,34 @@
-class Lacamento {
-    constructor(nome = "GEnerico", valor = 0){
+class Lancamento {
+    constructor(nome = "Generico", valor = 0) {
         this.nome = nome
         this.valor = valor
-
     }
 }
 
-class CicloFinaceiro {
-    constructor(mes,ano){
+class CicloFinanceiro {
+    constructor(mes, ano) {
         this.mes = mes
         this.ano = ano
-        this.Lacamento = []
+        this.lancamentos = []
     }
-    addLancamentos(...lancamentos) {
+
+    addLancamentos(...lancamentos) { 
         lancamentos.forEach(l => this.lancamentos.push(l))
     }
-    sumario(){
-        let valorConsolidado = 0 
-        this.lancamentos.forEach( l => {
+
+    sumario() {
+        let valorConsolidado = 0
+        this.lancamentos.forEach(l => {
             valorConsolidado += l.valor
         })
         return valorConsolidado
     }
 }
 
-const salario = new Lacamento ('Salario', 45000)
-const contaDeLuz = new  Lacamento('Luz', -220)
+const salario = new Lancamento("Salario", 45000)
+const contaDeLuz = new Lancamento("Luz", -220)
 
-const contas = new CicloFinaceiro(6, 2018)
+
+const contas = new CicloFinanceiro(6, 2018)
 contas.addLancamentos(salario, contaDeLuz)
 console.log(contas.sumario())
